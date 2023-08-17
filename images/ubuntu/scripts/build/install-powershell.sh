8aea99868e01e29cdc62e9b46c0d4ecea2280e0b
fffonion <fffonion@gmail.com>
2023-08-17 18:36:43 +0200

    [kvm] multi arch installer

--- images/ubuntu/scripts/build/install-powershell.sh
+++ images/ubuntu/scripts/build/install-powershell.sh
@@ -10,6 +10,31 @@ source $HELPER_SCRIPTS/os.sh
 
 pwsh_version=$(get_toolset_value .pwsh.version)
 
-# Install Powershell
+if [[ $(arch) == "aarch64" ]]; then
+	pwshversion=7.2.13
 
+	# Download the powershell '.tar.gz' archive
+	curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v$pwshversion/powershell-$pwshversion-linux-arm64.tar.gz
+
+	# Create the target folder where powershell will be placed
+	sudo mkdir -p /opt/microsoft/powershell/7
+
+	# Expand powershell to the target folder
+	sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+
+	# Set execute permissions
+	sudo chmod +x /opt/microsoft/powershell/7/pwsh
+
+	# Create the symbolic link that points to pwsh
+	sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+	exit 0
+fi
+
+if [[ "${ARCH}" == "arm64" ]]; then
+    ARCH_M="arm64"
+else
+    ARCH_M="x64"
+fi
+
+# Install Powershell
     apt-get install powershell=$pwsh_version*
