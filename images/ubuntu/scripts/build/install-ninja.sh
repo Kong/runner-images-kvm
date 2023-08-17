94c00825e4944ab0f0b2d390ce6b7bbf02a38a3e
fffonion <fffonion@gmail.com>
2023-08-17 18:36:52 +0200

    [kvm] arm64 base image

--- images/ubuntu/scripts/build/install-ninja.sh
+++ images/ubuntu/scripts/build/install-ninja.sh
@@ -8,7 +8,7 @@
 source $HELPER_SCRIPTS/install.sh
 
 # Install ninja
-download_url=$(resolve_github_release_asset_url "ninja-build/ninja" "endswith(\"ninja-linux.zip\")" "latest")
+download_url=$(resolve_github_release_asset_url "ninja-build/ninja" "endswith(\"ninja-linux-aarch64.zip\")" "latest")
 ninja_binary_path=$(download_with_retry "${download_url}")
 
 # Unzip the ninja binary
